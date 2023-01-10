55414f6a04f1852c4223dd37edd42e2af858736b
Gabriel Luiz Carpes <gabrielluizcm@hotmail.com>
2023-01-10 10:28:11 -0300

    Adding new items to the list

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import './Main.css';
 
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
+
 export default class Main extends Component {
   state = {
     newItem: '',
+    items: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { items } = this.state;
+    let { newItem } = this.state;
+    newItem = newItem.trim();
+
+    if (items.indexOf(newItem) !== -1) return;
+
+    const newItems = [...items];
+    this.setState({
+      items: [...newItems, newItem],
+    });
   };
 
   handleChange = (e) => {
@@ -13,14 +30,28 @@ export default class Main extends Component {
   };
 
   render() {
+    const { newItem, items } = this.state;
     return (
       <div className="main">
         <h1>CheckList</h1>
-
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Create item</button>
+        <form onSubmit={this.handleSubmit} className="form">
+          <input onChange={this.handleChange} type="text" value={newItem} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="items">
+          {items.map((item) => (
+            <li key={item}>
+              {item}
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
